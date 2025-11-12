5f04da470c43fade6fe5220eb9c8347ca623cdc9
AbhishekBiswas76 <abhishekbiswas7500@gmail.com>
2025-11-12 23:38:51 +0530

    I added code and head files

--- README.md
+++ README.md
@@ -20,7 +20,6 @@ Firstly, we would like to thank our family and friends for their constant suppor
 Moreover, we would like to express our sincere gratitude to the Department of Computer Science, for allowing us to apply our expertise in this assignment, and for helping us develop the required knowledge to program using the C programming language for this project.
 
 
-
 # Errors Found:
 
 
---
+++ original_fib_heap.h
@@ -0,0 +1,38 @@
+#ifndef FIB_HEAP_H
+#define FIB_HEAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct FibNode {
+    int key;
+    int degree;
+    struct FibNode *parent;
+    struct FibNode *child;
+    struct FibNode *left;
+    struct FibNode *right;
+    int mark; /* 0 or 1 */
+} FibNode;
+
+typedef struct FibHeap {
+    FibNode *min;
+    int n; /* number of nodes */
+} FibHeap;
+
+/* creation */
+FibHeap* make_fib_heap();
+FibNode* make_fib_node(int key);
+
+/* basic operations */
+FibNode* fib_heap_insert(FibHeap *H, int key);
+FibNode* fib_heap_min(FibHeap *H);
+FibHeap* fib_heap_union(FibHeap *H1, FibHeap *H2);
+FibNode* fib_heap_extract_min(FibHeap *H);
+void fib_heap_decrease_key(FibHeap *H, FibNode *x, int k);
+void fib_heap_delete(FibHeap *H, FibNode *x);
+
+/* utility / debug */
+void fib_heap_print(FibHeap *H);
+void fib_heap_free(FibHeap *H);
+
+#endif /* FIB_HEAP_H */
